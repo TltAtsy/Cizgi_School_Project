dfe3dd5be9db2a8aff0101d3d1186b9959a5f687
Çizgi Yazılım <izgi Yazlm@DESKTOP-594H67C>
2022-12-07 14:43:37 +0300

    Proje dosyası ekle.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cizgi_School_Project.Models;
+
+namespace Cizgi_School_Project.Controllers
+{
+    public class StudentController : Controller
+    {
+        CizgiSchoolDbEntities db = new CizgiSchoolDbEntities();
+        public ActionResult Index()
+        {
+            var values=db.TblStudent.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddStudent()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult AddStudent(TblStudent p)
+        {
+            db.TblStudent.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteStudent(int id)
+        {
+            var values = db.TblStudent.Find(id);
+            db.TblStudent.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
